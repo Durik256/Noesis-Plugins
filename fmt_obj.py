3eba4746d26cfc978efd55f86c273075a802840e
Durik256 <61597745+Durik256@users.noreply.github.com>
2024-04-16 15:07:10 +0300

    Moorhuhn Kart Extra [*.obj]

--- fmt_obj.py
+++ fmt_obj.py
@@ -5,7 +5,7 @@ def registerNoesisTypes():
     handle = noesis.register("Moorhuhn Kart Extra", ".obj")
     noesis.setHandlerTypeCheck(handle, noepyCheckType)
     noesis.setHandlerLoadModel(handle, noepyLoadModel)
-    noesis.logPopup()
+    #noesis.logPopup()
     return 1
 
 def noepyCheckType(data):
