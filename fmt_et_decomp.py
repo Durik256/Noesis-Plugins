1ebf7dbf6290e7d72794d0c842aea19d3132e655
Durik256 <61597745+Durik256@users.noreply.github.com>
2024-04-17 15:41:15 +0300

    ben 10 omniverse [*.et_decomp]

--- fmt_et_decomp.py
+++ fmt_et_decomp.py
@@ -5,7 +5,7 @@ def registerNoesisTypes():
     handle = noesis.register("ben 10 omniverse", ".et_decomp")
     noesis.setHandlerTypeCheck(handle, noepyCheckType)
     noesis.setHandlerLoadModel(handle, noepyLoadModel)
-    #noesis.logPopup()
+    noesis.logPopup()
     return 1
 
 def noepyCheckType(data):
@@ -66,13 +66,45 @@ def noepyLoadModel(data, mdlList):
             rapi.rpgClearBufferBinds()
             print('ERROR::', x)
 
+    texList = []
+    LoadTexture(data, bs, texList)
+
     try:
         mdl = rapi.rpgConstructModel()
     except:
         mdl = NoeModel()
-
+    
+    mdl.setModelMaterials(NoeModelMaterials(texList, [NoeMaterial('default','')]))
     mdlList.append(mdl)
     return 1
+    
+def LoadTexture(data, bs, texList):
+    result = [i for i in findall(b'\x00\x01\x00\x00\x00\x00\x00\x00\xBB\xBB\xBB\xBB', data)]
+    print('tx_result:', result)
+    for x in result:
+        bs.seek(x+16)
+        if not bs.readUByte() and 257 == bs.readUInt():
+            width, height, fmt, size = bs.read('>4I')
+            print('tx::', width, height, fmt, size)
+            data = bs.read(size)
+            bs.seek(x-72)
+            name = bs.read(36).replace(b'\x00',b'').decode('ascii','ignore')
+            if fmt == 11:
+                fmt = noesis.NOESISTEX_DXT1
+                if size >= (width * height):
+                    fmt = noesis.NOESISTEX_DXT5
+            elif fmt == 10:
+                data = rapi.imageDecodeDXT(data, width, height, noesis.FOURCC_DXT5)
+                fmt = noesis.NOESISTEX_RGBA32
+            elif fmt == 9:
+                #FOURCC_BC5
+                data = rapi.imageDecodeDXT(data, width, height, noesis.FOURCC_BC3)
+                fmt = noesis.NOESISTEX_RGBA32
+            else:
+                print('unknow fmt', fmt)
+                continue
+
+            texList.append(NoeTexture(name, width, height, data, fmt))
 
 def findall(p, s):
     i = s.find(p)
