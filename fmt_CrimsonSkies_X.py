05c072b7fab01104eeefa895cab1f6a60fb99bdf
Durik256 <61597745+Durik256@users.noreply.github.com>
2024-04-23 23:21:55 +0300

    Crimson Skies (Xbox) [*.x]

---
+++ fmt_CrimsonSkies_X.py
@@ -0,0 +1,110 @@
+#by Durik256
+from inc_noesis import *
+
+def registerNoesisTypes():
+    handle = noesis.register("Crimson Skies (Xbox)", ".x")
+    noesis.setHandlerTypeCheck(handle, noepyCheckType)
+    noesis.setHandlerLoadModel(handle, noepyLoadModel)
+    #noesis.logPopup()
+    return 1
+
+def noepyCheckType(data):
+    return 1
+
+def noepyLoadModel(data, mdlList):
+    ctx = rapi.rpgCreateContext()
+    bs = NoeBitStream(data)
+    rapi.rpgSetOption(noesis.RPGOPT_SWAPHANDEDNESS, 1)
+    
+    global nodes, materials
+    nodes, materials = [], []
+    
+    bs.seek(32)
+    readNode(bs)
+    
+    try:
+        mdl = rapi.rpgConstructModel()
+    except:
+        mdl = NoeModel()
+    
+    if nodes:
+        nodes = rapi.multiplyBones(nodes)
+    
+    mdl.setBones(nodes)
+    mdl.setModelMaterials(NoeModelMaterials([], materials))
+    mdlList.append(mdl)
+    return 1
+    
+def readNode(bs,parent=None):
+    name = readString(bs)
+    trfm = NoeMat43.fromBytes(bs.read(48)).transpose()
+    bbox = bs.read('6f')
+    bs.read(4)# 3
+    
+    rapi.rpgSetName(name)
+    rapi.rpgSetTransform(trfm) 
+    print(name, [bs.tell()])
+    for x in range(bs.readUInt()):
+        readMesh(bs)
+    
+    nodes.append(NoeBone(len(nodes),name,trfm,parent))
+    
+    for x in range(bs.readUInt()):
+        readNode(bs,name)
+    
+def readMesh(bs):
+    u0 = bs.readUInt()
+    clr = bs.read('4B')
+    u1 = bs.readUInt()
+    u2 = bs.read('>i')[0]
+
+    print('unk:',u0,clr,u1,u2)
+    mat_name = 'mat_%i'%len(materials)
+    mat = NoeMaterial(mat_name, '')
+    mat.setDiffuseColor(NoeVec4([clr[0]/255,clr[1]/255,clr[2]/255,clr[3]/255]))
+
+    dict = {0:0, 1:1, 3:2, 7:3, 17:2, 15:4, 19:3, 23:4, 31:5}
+    numTx = dict[u2]
+    
+    for x in range(numTx):
+        tx_name = readString(bs)
+        print('    ',tx_name)
+        
+        if not x: mat.setTexture(tx_name)
+    
+    materials.append(mat)
+    
+    vnum = bs.readShort()
+    print('vnum:',vnum,'vstart:',bs.tell())
+    vbuf = bs.read(vnum*32)
+    
+    print(bs.read('16B'))
+    if bs.readUByte():
+        bs.seek(bs.readShort() * 12 * 4, 1)
+       
+    inum = bs.readShort()
+    print('inum:',inum,'istart:',bs.tell())
+    ibuf = bs.read(inum*2)
+    
+    rapi.rpgSetMaterial(mat_name)
+    rapi.rpgBindPositionBuffer(vbuf, noesis.RPGEODATA_FLOAT, 32)
+    rapi.rpgBindUV1BufferOfs(vbuf, noesis.RPGEODATA_USHORT, 32, 16)
+    rapi.rpgCommitTriangles(ibuf, noesis.RPGEODATA_USHORT, inum, noesis.RPGEO_TRIANGLE)
+    rapi.rpgClearBufferBinds()
+
+    bs.seek(2, 1) #FF
+    strip = bs.readUShort()
+    bs.seek(strip*2, 1)
+
+    if bs.readByte() == 1:
+        size=16
+        numUnk= bs.readInt()
+    else:
+        size=28
+        numUnk= bs.readShort()
+    bs.seek(numUnk*size, 1) 
+    
+    print('end_mesh:', bs.tell())
+   
+def readString(bs):
+    return bs.read(bs.readUInt()).split(b'\x00')[0].decode('ascii', 'ignore')
