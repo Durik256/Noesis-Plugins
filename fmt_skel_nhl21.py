12d5f8710c6cdff791130051f59796f286dac142
Durik256 <61597745+Durik256@users.noreply.github.com>
2024-08-05 18:41:49 +0300

    NHL 21 (PS4) [*.skel]

--- fmt_skel_nhl21.py
+++ fmt_skel_nhl21.py
@@ -13,7 +13,11 @@ def noepyCheckType(data):
         return 0
     return 1   
 
+readMesh = False
 def noepyLoadModel(data, mdlList):
+    if readMesh:
+        meshes = readAllMesh()
+    
     bs = NoeBitStream(data)
     bs.seek(32)
     
@@ -23,6 +27,9 @@ def noepyLoadModel(data, mdlList):
     readNode(bs)
     
     mdl = NoeModel()
+    if readMesh:
+        mdl.meshes = meshes
+    
     mdl.setBones(bones)
     mdlList.append(mdl)
     return 1
@@ -31,12 +38,12 @@ def readNode(bs, parent=None):
     global bones
     
     label = bs.read(bs.readUInt()+1)
-    print(label)
+    #print(label)
     unk = bs.readUInt()#4
     name = bs.read(bs.readUInt()).decode()
-    print(name)
+    #print(name)
     flag = bs.read('6B')[-1]
-    print('flag:',flag)
+    #print('flag:',flag)
 
     if flag:
         unk = bs.readUInt()#2
@@ -71,9 +78,29 @@ def readNode(bs, parent=None):
         world_mat = NoeMat44.fromBytes(bs.read(64)).toMat43()
         unk_mat = NoeMat44.fromBytes(bs.read(64)).toMat43()
         unk = bs.readUInt()#0
+        bones.append(NoeBone(len(bones), name, world_mat, parent))
     
     numChild = bs.readUInt()
-    bones.append(NoeBone(len(bones), name, world_mat, parent))
-    
+
     for x in range(numChild):
         readNode(bs, name)
+
+def readAllMesh():
+    meshes = []
+    try:
+        import fmt_NHL21_mcd as mcd
+        dir = os.path.dirname(rapi.getInputName())
+        for x in os.listdir(dir):
+            #print('orig_name_file:',x)
+            name, ext = os.path.splitext(x)
+            if ext.lower() == '.mcd':
+                #print('name_file:',name)
+                data = rapi.loadIntoByteArray(os.path.join(dir, x))
+                _mdlList = []
+                mcd.noepyLoadModel(data, _mdlList)
+                for j,mesh in enumerate(_mdlList[0].meshes):
+                    mesh.name = '%s_%i'%(name,j)
+                    meshes.append(mesh)
+    except:
+        print('Error load all mesh')
+    return meshes
