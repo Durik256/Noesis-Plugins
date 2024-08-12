0fb1eb5870af8f4b15e03ce982613c28f1e40590
Durik256 <61597745+Durik256@users.noreply.github.com>
2024-08-13 00:31:01 +0300

    3d looney tunes dash [*.fmlb]

--- fmt_fmlb.py
+++ fmt_fmlb.py
@@ -1,88 +1,82 @@
+#by Durik256
 from inc_noesis import *
 
 def registerNoesisTypes():
     handle = noesis.register("3d looney tunes dash", ".fmlb")
     noesis.setHandlerTypeCheck(handle, noepyCheckType)
     noesis.setHandlerLoadModel(handle, noepyLoadModel)
-    
-    noesis.logPopup()
     return 1
 
 def noepyCheckType(data):
-    bs = NoeBitStream(data)
-    if noeAsciiFromBytes(bs.readBytes(4)) != '_FML':
+    if data[:4] != b'_FML':
         return 0
     return 1   
 	
 def noepyLoadModel(data, mdlList):
     bs = NoeBitStream(data)
-    bs.seek(16)#_FML + ver?
-    count = bs.readInt()
-    data_offset = bs.readInt()
-    unk = bs.readInt()
-    bs.seek(20,NOESEEK_REL)#END> + 16bytes unk
+    ctx = rapi.rpgCreateContext()
     
-    print("count:",count,"offset:",data_offset,"unk:",unk)
+    bs.seek(4)#_FML
+
+    # 0,1,2-ver(0,8,3); 3-numFile; 4-dataOfs; 5-dataSize(+4);
+    h = bs.read('6I')
+    print(h)
     
+    bs.seek(4,1)#END>
+    bs.seek(16,1) #zero
+
     subMeshes = []
-    for x in range(count):
-        #type: f(102)-file?; g(103)-texture; h(104)-material; k(107)-geometry; z(122)-sound; r(114)-array?
-        #q(113)-anim; 
-        type = bs.readUByte()
-        bs.seek(11 ,NOESEEK_REL)#zero
-        name = searchString(bs)
-        bs.seek(19-len(name) ,NOESEEK_REL)
-        bs.seek(44 ,NOESEEK_REL)#unk
-        offset = bs.readInt()+data_offset
-        bs.seek(8 ,NOESEEK_REL)#unk + >>
-        print("type",type,name,offset)
-        if type==107:
-            subMeshes.append(SubMesh(name, offset))
-    
+    for x in range(h[3]):
+        #102 - hier; 103 - txload; 104 - material; 107 - geo; 108 - prim; 113 - clip; 114 - aniplug; 123 - colorset
+        
+        # 0-type, 1,2 - zero
+        fh = bs.read('3I')
+        print('    ', fh)
+        name = bs.read(64).split(b'\x00')[0]
+        print('    ', name)
+        # 0-data_ofs, 1-size, 2-index; 3-ender
+        inf = bs.read('=2I2H')
+        print('    ', inf)
+
+        if fh[0]==107:
+            subMeshes.append(SubMesh(name, inf[0]+h[4]))
+
     if not len(subMeshes):
         print("Dont have mesh")
         return 0
-    
-    #bs.seek(data_offset) 
-    
-    meshes = []
+
     for x in subMeshes:
         bs.seek(x.offset)
-        print(bs.getOffset())
-        name = searchString(bs)
-        bs.seek(69-len(name),NOESEEK_REL)#seek 70
-        iCount = bs.readInt()
-        vCount = bs.readInt()
+        name = bs.read(70).split(b'\x00')[0].decode()
+
+        inum = bs.readInt()
+        vnum = bs.readInt()
         unk1 =  bs.readInt()
         unk2 =  bs.readInt()
-        bs.seek(126,NOESEEK_REL)#seek212
-        print(name,"iCount",iCount,"vCount",vCount,unk1,unk2)
-        indices = [bs.readUShort() for x in range(iCount)]
-        vert = []
-        if(bs.readShort() != 0):
-            bs.seek(-2,NOESEEK_REL)
-        for v in range(vCount):
-            vert.append(NoeVec3.fromBytes(bs.readBytes(12)))
-        
-        mesh = NoeMesh(indices, vert, name, "mat_0")
-        #meshes.append(mesh)
-        mdlList.append(NoeModel([mesh]))
-    
-    print(bs.getOffset())
+        bs.seek(126,1)#seek212
+        print(name,"inum:",inum,"vnum:",vnum,unk1,unk2)
 
-    #mdl = NoeModel(meshes)
-    #mdl.setModelMaterials(NoeModelMaterials([], [NoeMaterial("mat_0","")]))
-    #mdlList.append(mdl)
+
+        ibuf = bs.read(inum*2)
+        if bs.readShort():
+            bs.seek(-2,1)
+        vbuf = bs.read(vnum*12)
+
+        rapi.rpgSetName(name)
+        rapi.rpgBindPositionBuffer(vbuf , noesis.RPGEODATA_FLOAT, 12)
+        rapi.rpgCommitTriangles(ibuf, noesis.RPGEODATA_USHORT, inum, noesis.RPGEO_TRIANGLE)
+        rapi.rpgClearBufferBinds()
+    
+    print(bs.tell())
+
+    try:
+        mdl = rapi.rpgConstructModel()
+    except:
+        mdl = NoeModel()
+    #mdl.setModelMaterials(NoeModelMaterials([], [NoeMaterial("default","")]))
+    mdlList.append(mdl)
     return 1
-    
-def searchString(bs):
-    bytes = []
-    byte = None
-    while byte != 0:
-        byte = bs.readUByte()
-        bytes.append(byte)
-    return noeAsciiFromBytes(bytes)
-    
+
 class SubMesh:
     def __init__(self, name, offset):
         self.name=name
